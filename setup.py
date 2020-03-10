0faa4c6a04cfa83e29000483a5d6988752d64d22
eugenepy <tn00372136@gmail.com>
2020-03-10 11:29:06 +0800

    update

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
     install_requires=requires,
     packages=find_packages(exclude='test'),
     dependency_links=[
-        "git+https://github.com/EugenePY/gym-kafka.git@master=gym-kafka"
+        "git+https://github.com/EugenePY/gym-kafka.git@master=gym-kafka"]
 )
